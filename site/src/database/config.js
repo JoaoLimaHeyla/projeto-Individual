293957ca70a3d1c2dcf6dbd56bf22f66eeeb38b0
isabelasaorinakano <isabela.nakano@sptech.school>
2022-11-30 11:06:53 -0300

    alterações css

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "earthbound",
-    user: "root",
-    password: "!4321",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
